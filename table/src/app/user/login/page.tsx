6b0fc527ac9ad21d11368007454ddc5497744f76
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-10-23 18:58:23 +0300

    add form state

--- table/src/app/user/login/page.tsx
+++ table/src/app/user/login/page.tsx
@@ -1,52 +1,87 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faCheck, faKey } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    usernam: "",
+    password: "",
+  });
+
+  const [isError, setIsError] = useState(false);
+
+  const changeFormData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const submitFromData = async (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="is-flex-direction-column tile is-8 container">
-      <div className="field">
-        <label className="label">Username</label>
-        <div className="control has-icons-left has-icons-right">
-          <input
-            className="input"
-            type="text"
-            placeholder="Username"
-            value=""
-          />
-          <span className="icon is-small is-left">
-            <FontAwesomeIcon className="tile is-5" icon={faUser} />
-          </span>
-          <span className="icon is-small is-right">
-            <FontAwesomeIcon className="tile is-5" icon={faCheck} />
-          </span>
+      <form action="" method="POST" onSubmit={submitFromData}>
+        <div className="field">
+          <label className="label">Username</label>
+          <div className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={formData.usernam}
+              onChange={changeFormData}
+            />
+            <span className="icon is-small is-left">
+              <FontAwesomeIcon className="tile is-5" icon={faUser} />
+            </span>
+            <span className="icon is-small is-right">
+              <FontAwesomeIcon className="tile is-5" icon={faCheck} />
+            </span>
+          </div>
         </div>
-        <p className="help is-danger">This username does not exsists</p>
-      </div>
 
-      <div className="field">
-        <label className="label">Email</label>
-        <div className="control has-icons-left has-icons-right">
-          <input
-            className="input"
-            type="password"
-            placeholder="Password"
-            value=""
-          />
-          <span className="icon is-small is-left">
-            <FontAwesomeIcon className="tile is-5" icon={faKey} />
-          </span>
-          <span className="icon is-small is-right">
-            <FontAwesomeIcon className="tile is-5" icon={faCheck} />
-          </span>
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control has-icons-left has-icons-right">
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={changeFormData}
+            />
+            <span className="icon is-small is-left">
+              <FontAwesomeIcon className="tile is-5" icon={faKey} />
+            </span>
+            <span className="icon is-small is-right">
+              <FontAwesomeIcon className="tile is-5" icon={faCheck} />
+            </span>
+          </div>
         </div>
-        <p className="help is-danger">This password is invalid</p>
-      </div>
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link">Log In</button>
+        {isError ? (
+          <div className="mb-2">
+            <p className="help is-size-6 is-danger">
+              Verify your username and password for accuracy, including letter
+              case and typos.
+            </p>
+          </div>
+        ) : null}
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link" type="submit">
+              Log In
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
